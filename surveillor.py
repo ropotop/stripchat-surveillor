2aa11943be26eecc8b723bce2fc4ba9780d5c22a
rabaukeronny <rabaukeronny@protonmail.com>
2022-02-04 22:48:08 +0100

    i forgot what I added but its pretty cool

--- surveillor.py
+++ surveillor.py
@@ -7,37 +7,54 @@ import sys
 import threading
 from datetime import datetime
 from time import sleep
+import logging
 
 import ffmpy
 import requests
 
 
-def m3u8_link_recorder(m3u8_link: str, model_username: str):
+def logit(message: str):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    handler = logging.FileHandler("surveil.log")
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.info(f"{datetime_tag()}: {message}")
+
+
+def datetime_tag():
+    return datetime.now().strftime("%y%m%d_%H%M%S")
+
+
+def m3u8_link_recorder(m3u8_link: str, model_username: str, sleep_time: int):
     """records video through m3u8 link. Is called by concurrent_stream_recording 
     method to be executed once per m3u8 link in parallel.
     """
 
     vids_preprocessed_dir = "vids_preprocessed"
     model_path = os.path.join(vids_preprocessed_dir, model_username)
-    datetime_tag = datetime.now().strftime("%y%m%d_%H%M%S")
-    vid_name = f"{datetime_tag}.mkv"
+    vid_name = f"{datetime_tag()}.mkv"
     vid_path = os.path.join(vids_preprocessed_dir, model_username, vid_name)
     ff = ffmpy.FFmpeg(
         inputs={m3u8_link: None},
         outputs={vid_path: "-c copy"}
     )
 
-    if os.path.isdir(vids_preprocessed_dir) == False:
+    if not os.path.isdir(vids_preprocessed_dir):
         os.mkdir(vids_preprocessed_dir)
-    if os.path.isdir(model_path) == False:
+    if not os.path.isdir(model_path):
         os.mkdir(model_path)
+
+    logit(f"{model_username} is being recorded")
 
     thread_1 = threading.Thread(target=ff.run)
     thread_1.start()
     while not ff.process:
-        sleep(600)
+        sleep(sleep_time)
     ff.process.terminate()
     thread_1.join()
+
+    logit(f"{model_username} recording stopped")
 
 
 def model_list_grabber():
@@ -62,6 +79,7 @@ def model_list_grabber():
         models_online_resolution_option_480p.append(
             tuple([id, uname, resolution_option_480p]))
 
+
     return models_online_resolution_option_480p, models
 
 
@@ -70,11 +88,10 @@ def model_list_saver(model_list):
     """
 
     data_dump_dir = "data_dump"
-    datetime_tag = datetime.now().strftime("%y%m%d_%H%M%S")
-    json_file_name = f"{datetime_tag}.json"
+    json_file_name = f"{datetime_tag()}.json"
     json_file_path = os.path.join(data_dump_dir, json_file_name)
 
-    if os.path.isdir(data_dump_dir) == False:
+    if not os.path.isdir(data_dump_dir):
         os.mkdir(data_dump_dir)
     with open(json_file_path, "w") as fp:
         json.dump(model_list, fp)
@@ -103,30 +120,30 @@ def stream_download_decider(all_model_names_480_option: tuple):
                         tuple([id_online, uname_online, option_480p_online]))
     if len(models_followed_online) > 0:
         print(models_followed_online)
+        
     elif len(models_followed_online) == 0:
         print("none of your models are online")
 
     return models_followed_online
 
 
-def concurrent_stream_recording(models_online_followed: tuple):
+def concurrent_stream_recording(models_online_followed: tuple, sleep_time: int, models_to_record: int):
     """Invokes concurrent library.
     """
 
-    models_to_record = 8
     m3u8_links = []
     usernames = [x[1] for x in models_online_followed]
 
     for id, uname, option_480p in models_online_followed:
-        if option_480p == True:
+        if option_480p:
             m3u8_link = f"https://b-hls-01.strpst.com/hls/{id}/{id}_480p.m3u8"
             m3u8_links.append(m3u8_link)
-        elif option_480p == False:
+        elif not option_480p:
             m3u8_link = f"https://b-hls-01.strpst.com/hls/{id}/{id}.m3u8"
             m3u8_links.append(m3u8_link)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         executor.map(m3u8_link_recorder,
-                     m3u8_links[:models_to_record], usernames[:models_to_record])
+                     m3u8_links[:models_to_record], usernames[:models_to_record], [sleep_time] * models_to_record)
 
 
 def video_stitcher():
@@ -137,14 +154,15 @@ def video_stitcher():
     vids_preprocessed_dir = "vids_preprocessed"
     subdirs = os.listdir(vids_preprocessed_dir)
 
+    logit(f"video_stitcher started")
+
     for subdir in subdirs:
         dir_and_subdir = os.path.join(vids_preprocessed_dir, subdir)
         if len(os.listdir(dir_and_subdir)) > 1:
-            datetime_tag = datetime.now().strftime("%y%m%d_%H%M%S")
             vids = os.listdir(dir_and_subdir)
             list_txt_dir = os.path.join(dir_and_subdir, "my_list.txt")
             output_dir = os.path.join(
-                dir_and_subdir, f"concat_{datetime_tag}.mkv")
+                dir_and_subdir, f"concat_{datetime_tag()}.mkv")
 
             with open(list_txt_dir, "w") as fp:
                 for vid in vids:
@@ -162,14 +180,17 @@ def video_stitcher():
                 vid_dir = os.path.join(dir_and_subdir, vid)
                 os.remove(vid_dir)
             os.remove(list_txt_dir)
+            logit(f"video_stitcher concatenated {subdir}")
 
 
 def main():
     while True:
-        for i in range(6):
+        for i in range(3):
             models_online, models = model_list_grabber()
+            logit(f"{len(models_online)} followed models are online")
             models_online_followed = stream_download_decider(models_online)
-            concurrent_stream_recording(models_online_followed)
+            logit(f"{len(models_online_followed)} followed models are online")
+            concurrent_stream_recording(models_online_followed, 150, 8)
         video_stitcher()
 
 
